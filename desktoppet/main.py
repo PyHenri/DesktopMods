be4a16acb9d76678e7d9ae5c1c3f7343194ca248
PyHenri <95569320+PyHenri@users.noreply.github.com>
2022-11-23 18:05:28 +0100

    Update main.py

--- desktoppet/main.py
+++ desktoppet/main.py
@@ -34,31 +34,58 @@ class pet:
         self.activity = random.choice(["idle", "walk_positive", "walk_negative", "sleep"])
         self.timestamp = time.time()
 
+
     def update(self):
         if time.time() > self.timestamp + 0.05:
             self.timestamp = time.time()
-        
+
         if self.activity == "walk_positive" and self.pos[0] + 5 <= SWidth - 100:
             self.pos[0] += 5
 
-        elif self.activity == "walk_negativ" and self.pos[0] - 5 >= 0:
+        elif self.activity == "walk_negative" and self.pos[0] - 5 >= 0:
             self.pos[0] -= 5
             
         if self.activity == "idle":
             self.frame_index = (self.frame_index + 1) % len(self.idle)
             self.img = self.idle[self.frame_index]
-
-        elif self.activity == "walk_positive":
-            self.frame_index = (self.frame_index + 1) % len(self.walk_positive)
-            self.img = self.walk_positive[self.frame_index]
-
-        elif self.activity == "walk_negative":
-            self.frame_index = (self.frame_index + 1) % len(self.walk_negative)
-            self.img = self.walk_negative[self.frame_index]
+            if self.frame_index == len(self.idle)-1:
+                self.activity = random.choice(["idle", "walk_positive", "walk_negative", "idle_to_sleep"] + ["idle"] * 10)
 
         elif self.activity == "sleep":
             self.frame_index = (self.frame_index + 1) % len(self.sleep)
             self.img = self.sleep[self.frame_index]
+            if self.frame_index == len(self.sleep)-1:
+                self.activity = random.choice(["sleep", "sleep_to_idle"] + ["sleep"] * 10)
+        
+        elif self.activity == "walk_positive":
+            self.frame_index = (self.frame_index + 1) % len(self.walk_positive)
+            self.img = self.walk_positive[self.frame_index]
+            if self.frame_index == len(self.walk_positive)-1:
+                if self.pos[0] + 5 <= SWidth - 100:
+                    self.activity = random.choice(["idle", "walk_positive", "walk_negative"] + ["walk_positive"] * 8)
+                else:
+                    self.activity = random.choice(["idle", "walk_positive", "walk_negative"] + ["walk_negative"] * 3)
+
+        elif self.activity == "walk_negative":
+            self.frame_index = (self.frame_index + 1) % len(self.walk_negative)
+            self.img = self.walk_negative[self.frame_index]
+            if self.frame_index == len(self.walk_negative)-1:
+                if self.pos[0] - 5 >= 0:
+                    self.activity = random.choice(["idle", "walk_positive", "walk_negative"] + ["walk_negative"] * 8)
+                else:
+                    self.activity = random.choice(["idle", "walk_positive", "walk_negative"] + ["walk_positive"] * 3)
+        
+        elif self.activity == "idle_to_sleep":
+            self.frame_index = (self.frame_index + 1) % len(self.idle_to_sleep)
+            self.img = self.idle_to_sleep[self.frame_index]
+            if self.frame_index == len(self.idle_to_sleep)-1:
+                self.activity = "sleep"
+        
+        elif self.activity == "sleep_to_idle":
+            self.frame_index = (self.frame_index + 1) % len(self.sleep_to_idle)
+            self.img = self.sleep_to_idle[self.frame_index]
+            if self.frame_index == len(self.sleep_to_idle)-1:
+                self.activity = "idle"
         
         window.geometry(f"100x100+{self.pos[0]}+{self.pos[1]}")
 
